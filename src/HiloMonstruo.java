0ab8e266ef7bea8103cd133584d4a8da0b216f3a
David-Francesch <david.francesch.colomer@gmail.com>
2022-11-09 12:26:53 +0100

    intento de algo

--- src/HiloMonstruo.java
+++ src/HiloMonstruo.java
@@ -1,3 +1,23 @@
+import java.util.concurrent.Semaphore;
+
 public class HiloMonstruo extends Thread{
+
+    Semaphore almacen = null;
+
+    public HiloMonstruo(Semaphore almacen){
+        super();
+        this.almacen = almacen;
+    }
+
+    public void run(){
+        try {
+            System.out.println("Quiero entrar" + Thread.currentThread().getId());
+            almacen.acquire();
+            
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
+    //metodo de comer galletas
     
 }
